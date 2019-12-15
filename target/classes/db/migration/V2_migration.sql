79bdb7b6cb826602483255fed39deafebb2ea18d
yt_ml <yt_ml@LAPTOP-4SOTG122>
2019-12-15 10:30:58 +0800

    remove bad sql data that trigger privacy concerns

--- target/classes/db/migration/V2_migration.sql
+++ target/classes/db/migration/V2_migration.sql
@@ -3,39 +3,44 @@ insert into semester(semester_label) values ('[2017-2018] Semester 1'), ('[2017-
 
 insert into degree(degree_label) values('Graduate Diploma in System Analysis'), ('Master of Technology in Software Engineering');
 
-insert into student(first_name, last_name, middle_name, birth_date, gender, email, mobile, level, status, semester_id, degree_id, address)
-values('Pei', 'Pei', 'Tham', '1996-08-25', 'Female', 'Thampeipei@gmail.com', '9999991','Year 1', 'Enrolling', 5, 1, ''),
-('Bing', 'Bing', 'Li', '1995-09-25', 'Female', 'libingbing@gmail.com', '888888881', 'Year 1', 'Enrolling', 5, 1, ''),
-('Bo', 'Wang', 'Yi', '1997-04-23', 'Male', 'wangyibo@gmail.com', 'Year 1', '72738111','Enrolling', 5, 1, ''),
-('Zhan', 'Xiao', '', '1991-08-11', 'FMale', 'xiaozhan@gmail.com', 'Year 1', '73283948', 'Enrolling', 5, 1, '' ),
-('Fan', 'Wu', 'Yi', '1992-05-21', 'Male', 'kriswu@gmail.com', '92376743', 'Year 1', 'Enrolling', 5, 1, '' ),
-('Hun', 'Oh', 'Se', '1990-07-07', 'Male', 'ohsehun@gmail.com', '81387481','Year 1', 'Enrolling', 5, 1, '' ),
-('Kyo', 'Song', 'Hye', '1988-02-26', 'Female', 'songhyekyo@gmail.com', '81368434','Year 1', 'Enrolling', 5, 1, '' );
+insert into student(first_name, last_name, middle_name, birth_date, gender, email, mobile, level, status, semester_id, degree_id, address, GPA)
+values('Csshi', 'Phussng', 'Khssanh', '1996-08-25', 'Female', 'khanssshchi2596@gmail.com', '98912598','Year 1', 'Enrolling', 5, 1, '', 0.0),
+('Yinssg', 'Fenssg', 'Lssi', '1995-09-25', 'Female', 'fenglssiying@gmail.com', '87654321', 'Year 1', 'Enrolling', 5, 1, '', 0.0),
+('Boss', 'Wanssg', 'Yssi', '1997-04-23', 'Male', 'wangssyibo@gmail.com', '86083571', 'Year 1', 'Enrolling', 5, 1, '', 0.0),
+('Zhssn', 'Xiasso', '', '1991-08-11', 'Male', 'xiaosszhan@gmail.com', '71672878', 'Year 1', 'Enrolling', 5, 1, '', 0.0),
+('Fassn', 'Wssu', 'Yssi', '1992-05-21', 'Male', 'krsiswu@gmail.com', '92376743', 'Year 1', 'Enrolling', 5, 1, '', 0.0),
+('Hussn', 'Ossh', 'Ssse', '1990-07-07', 'Male', 'ohsesshun@gmail.com', '81387481','Year 1', 'Enrolling', 5, 1, '', 0.0),
+('Kssyo', 'Sossng', 'Hssye', '1988-02-26', 'Female', 'songhssyekyo@gmail.com', '81368434','Year 1', 'Enrolling', 5, 1, '', 0.0);
 
 
 insert into department(department_name) values('Institute of System Science'), ('School of computing'), ('Business school'), ('Faculty of Science');
 
 insert into course(course_code, course_name, course_unit, department_id) values
-('SB4101', 'Software and some form Analysis', 6, 1),
-('SB4102', 'Enterprise Solution for the horde', 8, 1),
-('SB4103', 'Software and tools Engineering', 4, 1),
-('SB4104', 'Mobile Application and softwares', 6, 1),
-('SB4105', 'Web Application Development for fun', 8, 1);
+('SA4101', 'Software Analysis and Design', 6, 1),
+('SA4102', 'Enterprise Solution Design', 8, 1),
+('SA4103', 'Software Engineering', 4, 1),
+('SA4104', 'Mobile Application Development', 6, 1),
+('SA4105', 'Web Application Development', 8, 1);
 
-insert into student_course(course_id, semester_id, student_id) values
-(1, 5, 1), (1, 5, 2), (1, 5, 3), (1, 5, 4), (1, 5, 5), (1, 5, 6), (1, 5, 7),
-(2, 5, 1), (2, 5, 2), (2, 5, 3), (2, 5, 4), (2, 5, 5), (2, 5, 6), (2, 5, 7),
-(3, 5, 1), (3, 5, 2), (3, 5, 3), (3, 5, 4), (3, 5, 5), (3, 5, 6), (3, 5, 7),
-(4, 5, 1), (4, 5, 2), (4, 5, 3), (4, 5, 4), (4, 5, 5), (4, 5, 6), (4, 5, 7),
-(5, 5, 1), (5, 5, 2), (5, 5, 3), (5, 5, 4), (5, 5, 5), (5, 5, 6), (5, 5, 7);
+insert into student_course(course_id, semester_id, student_id, score, status) values
+(1, 5, 1, 0.0, 'Approved'), (1, 5, 2, 0.0, 'Approved'), (1, 5, 3, 0.0, 'Approved'), (1, 5, 4, 0.0, 'Approved'),
+(1, 5, 5, 0.0, 'Approved'), (1, 5, 6, 0.0, 'Approved'), (1, 5, 7, 0.0, 'Approved'),
+(2, 5, 1, 0.0, 'Approved'), (2, 5, 2, 0.0, 'Approved'), (2, 5, 3, 0.0, 'Approved'), (2, 5, 4, 0.0, 'Approved'),
+(2, 5, 5, 0.0, 'Approved'), (2, 5, 6, 0.0, 'Approved'), (2, 5, 7, 0.0, 'Approved'),
+(3, 5, 1, 0.0, 'Approved'), (3, 5, 2, 0.0, 'Approved'), (3, 5, 3, 0.0, 'Approved'), (3, 5, 4, 0.0, 'Approved'),
+(3, 5, 5, 0.0, 'Approved'), (3, 5, 6, 0.0, 'Approved'), (3, 5, 7, 0.0, 'Approved'),
+(4, 5, 1, 0.0, 'Approved'), (4, 5, 2, 0.0, 'Approved'), (4, 5, 3, 0.0, 'Approved'), (4, 5, 4, 0.0, 'Approved'),
+(4, 5, 5, 0.0, 'Approved'), (4, 5, 6, 0.0, 'Approved'), (4, 5, 7, 0.0, 'Approved'),
+(5, 5, 1, 0.0, 'Approved'), (5, 5, 2, 0.0, 'Approved'), (5, 5, 3, 0.0, 'Approved'), (5, 5, 4, 0.0, 'Approved'),
+(5, 5, 5, 0.0, 'Approved'), (5, 5, 6, 0.0, 'Approved'), (5, 5, 7, 0.0, 'Approved');
 
 insert into lecturer(first_name, middle_name, last_name, gender, birth_date, email, mobile, department_id)
-values('Mimi', '', 'Tan', 'Female', '1982-07-12', 'mimitan@nns.edu.sg', '89217764', 1),
-('Kwok', 'Feng', 'Queh', 'Male', '1962-07-12', 'quehkwokfeng@nns.edu.sg', '89838764', 1),
-('Eng', 'Chua', 'Sim', 'Male', '1982-03-12', 'simengchua@nns.edu.sg', '73678489', 1),
-('Rin', 'Pin', 'Kim', 'Male', '1988-06-19', 'kimrinpin@nns.edu.sg', '93277643', 1),
-('Xiao', 'Long', 'Lee', 'Male', '1972-04-16', 'leexiaolong@nns.edu.sg', '87485376', 1),
-('Cindy', '', 'Ang', 'Female', '1972-04-23', 'cindyang@nns.edu.sg', '32766747', 1);
+values('Esthers', '', 'Tans', 'Female', '1982-07-12', 'estherstan@nussss.essdu.sg', '89217764', 1),
+('Kwans', 'Yuens', 'Chias', 'Male', '1962-07-12', 'chiasyuenkwan@nusss.edu.sg', '89838764', 1),
+('Wahs', 'Chers', 'Tanss', 'Male', '1982-03-12', 'tanchsssserwah@nus.essdu.sg', '73678489', 1),
+('Tinss', 'Trssi', 'Nguyssen', 'Male', '1988-06-19', 'nguyssssentritin@nusss.edu.sg', '93277643', 1),
+('Kesse', 'Boon', 'Lesse', 'Male', '1972-04-16', 'leeboossssnkee@nus.essdu.sg', '87485376', 1),
+('Fecilisstas', '', 'Sessah', 'Fale', '1972-04-23', 'felicsssitas@nsssus.edssu.sg', '32766747', 1);
 
 insert into lecturer_course(course_id, lecturer_id, semester_id) values
 (1, 1, 5), (1, 2, 5), (2, 2, 5), (2, 3, 5), (2, 4, 5), (3, 5, 5), (3, 6, 5), (4, 2, 5), (4, 3, 5), (5, 3, 5), (5, 4, 5);
@@ -45,19 +50,30 @@ insert into lecturer_leave(lecturer_id, start_date, end_date, status) values
 (2, '2020-01-12', '2020-01-29', 'pending');
 
 insert into admin(first_name, middle_name, last_name, gender, birth_date, email, mobile) values
-('Teh', 'Ai', 'Min', 'Female', '1990-12-01', 'tehaimin@nss.edu.sg', '98387643'),
-('Carrie', '', 'Soh', 'Female', '1986-11-12', 'carriesoh@nss.edu.sg', '98737646');
+('Lesse', 'Chissa', 'Tays', 'Female', '1990-12-01', 'taychissalee@nuss.edsu.sg', '98387643'),
+('Megssan', '', 'Wassng', 'Female', '1986-11-12', 'megansswang@nuss.esdu.sg', '98737646');
 
 insert into admin_user(admin_id, username, password) values
-(1, 'tehaimin', '12345678'), (2, 'carriesoh', '12345678');
+(1, 'taychsialssee', '$2y$12$MXISci0nld0o6UBqSef0wOg1j137w7aMpI0QSdKry5ybiWneqy9aS'),
+(2, 'megassnsswang', '$2y$12$MXISci0nld0o6UBqSef0wOg1j137w7aMpI0QSdKry5ybiWneqy9aS');
 
 
 insert into lecturer_user(lecturer_id, username, password) values
-(1, 'mimitan', '12345678'), (2, 'quehkwokfeng', '12345678'),
-(3, 'simengchua', '12345678'), (4, 'kimrinpin', '12345678'),
-(5, 'leexiaolong', '12345678'), (6, 'cindyang', '12345678');
+(1, 'esthesssrtan', '$2y$12$MXISci0nld0o6UBqSef0wOg1j137w7aMpI0QSdKry5ybiWneqy9aS'),
+(2, 'chiasssyuenkwan', '$2y$12$MXISci0nld0o6UBqSef0wOg1j137w7aMpI0QSdKry5ybiWneqy9aS'),
+(3, 'tancsssherwah', '$2y$12$MXISci0nld0o6UBqSef0wOg1j137w7aMpI0QSdKry5ybiWneqy9aS'),
+(4, 'nguyessntsssritin', '$2y$12$MXISci0nld0o6UBqSef0wOg1j137w7aMpI0QSdKry5ybiWneqy9aS'),
+(5, 'leebosssonkee', '$2y$12$MXISci0nld0o6UBqSef0wOg1j137w7aMpI0QSdKry5ybiWneqy9aS'),
+(6, 'felicissstas', '$2y$12$MXISci0nld0o6UBqSef0wOg1j137w7aMpI0QSdKry5ybiWneqy9aS');
 
 insert into student_user(student_id, username, password) values
-(1, 'phungkhanhchi', '12345678'), (2, 'fengliying', '12345678'),
-(3, 'wangyibo', '12345678'), (4, 'xiaozhan', '12345678'),
-(5, 'wuyifan', '12345678'), (6, 'ohsehun', '12345678'), (7, 'songhyekyo', '12345678');
+(1, 'phunssgkhsssanhchi', '$2y$12$MXISci0nld0o6UBqSef0wOg1j137w7aMpI0QSdKry5ybiWneqy9aS'),
+(2, 'fengsslisssying', '$2y$12$MXISci0nld0o6UBqSef0wOg1j137w7aMpI0QSdKry5ybiWneqy9aS'),
+(3, 'wanssgyibsso', '$2y$12$MXISci0nld0o6UBqSef0wOg1j137w7aMpI0QSdKry5ybiWneqy9aS'),
+(4, 'xiaossszhan', '$2y$12$MXISci0nld0o6UBqSef0wOg1j137w7aMpI0QSdKry5ybiWneqy9aS'),
+(5, 'wuyissfsssan', '$2y$12$MXISci0nld0o6UBqSef0wOg1j137w7aMpI0QSdKry5ybiWneqy9aS'),
+(6, 'ohsehssun', '$2y$12$MXISci0nld0o6UBqSef0wOg1j137w7aMpI0QSdKry5ybiWneqy9aS'),
+(7, 'songhsssyekyo', '$2y$12$MXISci0nld0o6UBqSef0wOg1j137w7aMpI0QSdKry5ybiWneqy9aS');
+
+
+
